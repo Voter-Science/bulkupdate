44505bb3e9ff5ddcd9a336a0bfffc1a34165e5a7
Voter-Science <Info@Voter-Science.com>
2019-07-07 12:00:35 -0700

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,17 @@ import * as XC from 'trc-httpshim/xclient'
 import * as common from 'trc-httpshim/common'
 import * as core from 'trc-core/core'
 import * as trcSheet from 'trc-sheet/sheet'
-import { checkPropTypes } from "prop-types";
 
-
-// import { ColumnSelector } from "./components/ColumnSelector";
 import { SheetContainer, IMajorState } from 'trc-react/dist/SheetContainer'
-
 import { VrdbLookup, IVoter } from './components/VrdbLookup'
 
 import * as bcl from 'trc-analyze/collections'
-import { AllQuestions } from "trc-react/dist/Questions";
 import { PluginLink } from "trc-react/dist/PluginLink";
 import { ColumnNames } from "trc-sheet/sheetContents";
 
 
-import { CsvMatchInput} from 'trc-react/dist/CsvMatchInput'; // $$$
+import { CsvMatchInput} from 'trc-react/dist/CsvMatchInput';
+import { ListColumns } from 'trc-react/dist/ListColumns';
 
 declare var _trcGlobal: IMajorState;
 
@@ -36,59 +32,8 @@ export class App extends React.Component<{}, {
             voter: undefined
         };
         this.renderBody1 = this.renderBody1.bind(this);
-        this.renderPerVoter = this.renderPerVoter.bind(this);
         this.selectVoter = this.selectVoter.bind(this);
-        this.onSubmitAnswers = this.onSubmitAnswers.bind(this);
     }
-
-    private renderPerVoter() {
-        var v = this.state.voter;
-        return <div>
-            Fill in details for {v.FirstName} {v.LastName}:
-            <AllQuestions onSubmit={this.onSubmitAnswers}
-                columns={_trcGlobal._info.Columns}
-            ></AllQuestions>
-        </div>
-    }
-
-    private onSubmitAnswers(answers: any) {
-        // Post answers
-        var v = this.state.voter;
-        var recId = v.RecId;
-        var columnNames: string[] = [];
-        var newValues: string[] = [];
-
-        // Include VRDB information inthe delta table (since we don't have in contents).
-        if (v.FirstName) {
-            columnNames.push(ColumnNames.FirstName);
-            newValues.push(v.FirstName);
-        }
-        if (v.LastName) {
-            columnNames.push(ColumnNames.LastName);
-            newValues.push(v.LastName);
-        }
-        if (v.Birthdate) {
-            columnNames.push("Birthday");
-            newValues.push(v.Birthdate.toDateString());
-        }
-
-        for (var i in answers) {
-            columnNames.push(i);
-            newValues.push(answers[i]);
-        }
-
-        // Push result to server 
-        _trcGlobal.SheetClient.postUpdateSingleRowAsync(recId, columnNames, newValues).then(() => {
-            // Success. Now go back to looking up another voter.
-            alert("Successfully recorded");
-            this.setState({
-                voter: undefined
-            })
-        }).catch((err) => {
-            alert("Failed to post response: " + err);
-        });
-    }
-
     private selectVoter(record: IVoter) {
         this.setState({
             voter: record
@@ -97,27 +42,19 @@ export class App extends React.Component<{}, {
 
     private renderBody1() {
         {
-            return <CsvMatchInput></CsvMatchInput>
+            return <div>
+                <h2>Bulk Uploader</h2>            
+                <div>This lets you specify a CSV to bulk update values in the current sheet.</div>
+                <div>Column names for the CSV must match "RecId" (the primary key) and possible editable columns:</div>
+                <ListColumns Include={ci => !ci.IsReadOnly}></ListColumns>
+
+                <CsvMatchInput></CsvMatchInput>
+             </div>
         }
-
-
-        if (this.state.voter) {
-            return this.renderPerVoter();
-        }
-        return <div>
-            <p>Lookup voters in WA and then answer survey questions about them.</p>
-            <VrdbLookup onSelect={this.selectVoter}>
-            </VrdbLookup>
-
-            <div>
-                You can see results at <PluginLink id="Audit" url="#show=byrecid"/>
-            </div>
-        </div>
     }
 
     render() {
         return <div>
-
             <SheetContainer
                 onReady={this.renderBody1}
                 fetchContents={true}
